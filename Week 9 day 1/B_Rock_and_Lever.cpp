16ea74128b7952d9c8eb9babbd894b56b3c1b3aa
shafiqbd1st <131482599+shafiqbd1st@users.noreply.github.com>
2023-11-27 21:52:11 +0600

    Add files via upload

---
+++ Week 9 day 1/B_Rock_and_Lever.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define sorti(v) sort(v.begin(), v.end())
+#define sortd(v) sort(v.begin(), v.end(), greater<int>())
+
+const int N = 1e5 + 9;
+const int mod = 1e9 + 7;
+
+void solution()
+{
+    int n;
+    cin >> n;
+    int cnt[30] = {0};
+    
+    for(int i = 0; i < n; i++){
+        int item; cin >> item;
+        int k = __lg(item);
+        cnt[k]++;
+    }
+
+    ll ans = 0;
+    for(int i = 0; i < 30; i++){
+        ans += 1LL * cnt[i] * (cnt[i] - 1) / 2;
+    }
+    cout << ans << '\n';
+}
+
+int main()
+{
+    ios_base:: sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    cin >> t;
+    // cin.ignore();
+    for(int i = 1; i <= t; i++){
+        // cout << "Case " << i << ": ";
+        solution();
+    }
+    
+    return 0;
+}
